7673e89b23a706f2a83b6aafd3627223efb547c5
Dominik Hümbeli <dominik.huembeli@digitecgalaxus.ch>
2024-09-04 15:19:39 +0200

    Add cask

---
+++ Cask/dg.rb
@@ -0,0 +1,11 @@
+cask 'dg' do
+    version '1.0.0'
+    sha256 "83745cb9613d2a856de66650cf153caf1d83d6569ac98d9242dc297dc4ebeff2"
+  
+    url "https://dg-package-repositories.platform.test.int.devinite.com/macos/brew/dg-cli/dg-cli_2.0.0.dmg"
+    name 'Dg'
+    homepage "https://backstage.devinite.com/docs/default/component/dg.cli"
+  
+    app 'dg.app'
+    binary "#{appdir}/dg.app/Contents/MacOS/dg", target: 'dg'
+  end
