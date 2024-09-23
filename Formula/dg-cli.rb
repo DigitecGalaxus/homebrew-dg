2bed78110e2b3f7619a887e1c8b359e631938329
Dominik Hümbeli <dominik.huembeli@digitecgalaxus.ch>
2024-09-23 15:01:07 +0200

    Add sha and use curl for download

--- Formula/dg-cli.rb
+++ Formula/dg-cli.rb
@@ -2,7 +2,8 @@ class DgCli < Formula
   desc "Digitec Galaxus CLI"
   homepage "https://www.galaxus.ch"
   version "1.13.25"
-  url "https://dg-package-repositories.platform.prod.int.devinite.com/DGCLI/mac/dg-cli-1.13.25.tar.gz"
+  url "https://dg-package-repositories.platform.prod.int.devinite.com/DGCLI/mac/dg-cli-1.13.25.tar.gz", :using => :curl
+  sha256 "a0957bcddce9e1627557776bc455162405426d8a5670b5955704889aa9c7f980"
 
   def install
     libexec.install Dir["*"]
