9e6d77f7178a6a0eee9cc98cfeb244ee9b57b6a1
Automated Account <admin@azuredevops.com>
2024-10-29 15:16:35 +0000

    Update formula with new release.

--- Formula/dg-cli.rb
+++ Formula/dg-cli.rb
@@ -1,9 +1,9 @@
 class DgCli < Formula
   desc "Digitec Galaxus CLI"
   homepage "https://www.galaxus.ch"
-  version "2.1.0"
-  url "https://dg-package-repositories.platform.prod.int.devinite.com/DGCLI/mac/dg-cli-2.1.0.tar.gz", :using => :curl
-  sha256 "924dee2706e037e88f4e144c8a2f004bece3e7772ba2c1a84c7536afabeae939"
+  version "2.1.1"
+  url "https://dg-package-repositories.platform.prod.int.devinite.com/DGCLI/mac/dg-cli-2.1.1.tar.gz", :using => :curl
+  sha256 "10d4748dfbe04b6814ae364d98fa173a13b7db05c25bd0ab1de48ffb012a0f2a"
 
   def install
     libexec.install Dir["*"]
@@ -20,6 +20,6 @@ class DgCli < Formula
 
   test do
     version_text = shell_output("#{bin}/dg --version")
-    assert_equal version_text, "dg 2.1.0"
+    assert_equal version_text, "dg 2.1.1"
   end
 end
