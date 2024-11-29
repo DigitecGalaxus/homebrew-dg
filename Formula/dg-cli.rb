3371f3af3e2f598b4b6a2623d74211f723b26a21
Dominik Hümbeli <dominik.huembeli@digitecgalaxus.ch>
2024-11-29 15:50:15 +0100

    Next try

--- Formula/dg-cli.rb
+++ Formula/dg-cli.rb
@@ -5,6 +5,12 @@ class DgCli < Formula
   url "https://dg-package-repositories.platform.prod.int.devinite.com/DGCLI/mac/dg-cli-2.1.4.tar.gz", :using => :curl
   sha256 "ecbaf8ed410df12ff4096101a99e7207437d6e6de32aa6a002fe37ad137915ec"
 
+  # other versions
+  version "2.2.0-beta1" do
+    url "https://dg-package-repositories.platform.prod.int.devinite.com/DGCLI/mac/dg-cli-2.2.0-beta1.tar.gz", :using => :curl
+    sha256 "97093293210f01169c65385c07c3192736c4a564f23b6dca43913fb9369ef12d"
+  end
+
   def install
     libexec.install Dir["*"]
 
@@ -18,8 +24,4 @@ class DgCli < Formula
     system "xattr", "-rd", "com.apple.quarantine", "#{libexec}"
   end
 
-  test do
-    version_text = shell_output("#{bin}/dg --version")
-    assert_equal version_text, "dg 2.1.4"
-  end
 end
