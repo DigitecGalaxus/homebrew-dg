1fd2b158110a9b289994d6ae0df69cb0f2a45cc4
Automated Account <admin@azuredevops.com>
2024-12-04 06:21:23 +0000

    Update formula with new release.

--- Formula/dg-cli.rb
+++ Formula/dg-cli.rb
@@ -1,14 +1,14 @@
 class DgCli < Formula
   desc "Digitec Galaxus CLI"
   homepage "https://www.galaxus.ch"
-  version "2.2.2"
-  url "https://dg-package-repositories.platform.prod.int.devinite.com/DGCLI/mac/dg-cli-2.2.2.tar.gz", :using => :curl
-  sha256 "227349fdf528a302f90d6e1c3a9ee0361912dd9d9d4a24e4853e640a925138a4"
+  version "2.2.3"
+  url "https://dg-package-repositories.platform.prod.int.devinite.com/DGCLI/mac/dg-cli-2.2.3.tar.gz", :using => :curl
+  sha256 "7a32e1b3e0e9a3bab49498d3d61c297e11d7a469e5175862f24ab9ff8cf68a72"
 
   # other versions
-  version "2.2.2" do
-    url "https://dg-package-repositories.platform.prod.int.devinite.com/DGCLI/mac/dg-cli-2.2.2.tar.gz", :using => :curl
-    sha256 "227349fdf528a302f90d6e1c3a9ee0361912dd9d9d4a24e4853e640a925138a4"
+  version "2.2.3" do
+    url "https://dg-package-repositories.platform.prod.int.devinite.com/DGCLI/mac/dg-cli-2.2.3.tar.gz", :using => :curl
+    sha256 "7a32e1b3e0e9a3bab49498d3d61c297e11d7a469e5175862f24ab9ff8cf68a72"
   end
 
   def install
