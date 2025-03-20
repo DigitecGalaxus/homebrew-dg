b77db0525146071e7e8018e7fd525108ef069f9a
Automated Account <admin@azuredevops.com>
2025-03-20 08:58:55 +0000

    Update formula with new release.

--- Formula/dg-cli.rb
+++ Formula/dg-cli.rb
@@ -1,9 +1,9 @@
 class DgCli < Formula
   desc "Digitec Galaxus CLI"
   homepage "https://www.galaxus.ch"
-  version "2.4.0"
-  url "https://dg-package-repositories.platform.prod.int.devinite.com/DGCLI/mac/dg-cli-2.4.0.tar.gz", :using => :curl
-  sha256 "05f2a6eec0aeec14913e2091f33c9da080dd0349a2ec47800c097def2097e4fa"
+  version "2.4.1"
+  url "https://dg-package-repositories.platform.prod.int.devinite.com/DGCLI/mac/dg-cli-2.4.1.tar.gz", :using => :curl
+  sha256 "98ff57362d3e53993a9316035b4facb4f3856832939322e6b43ae520fb271799"
 
   depends_on "openssl@3"
 
