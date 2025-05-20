ba9a0cc106b54c33097c090177ca81da8c5c3667
Oliver Morf <48694510+MacGurk@users.noreply.github.com>
2025-05-20 10:45:12 +0200

    Update dg-cli-preview.rb

--- Formula/dg-cli-preview.rb
+++ Formula/dg-cli-preview.rb
@@ -1,8 +1,8 @@
 class DgCliPreview < Formula
   desc "Digitec Galaxus CLI"
   homepage "https://www.galaxus.ch"
-  version "2.5.2-b.3"
-  url "https://dg-package-repositories.platform.prod.int.devinite.com/DGCLI/mac/dg-cli-2.5.2-b.3.tar.gz", :using => :curl
+  version "2.5.2-b.4"
+  url "https://dg-package-repositories.platform.prod.int.devinite.com/DGCLI/mac/dg-cli-2.5.2-b.4.tar.gz", :using => :curl
   sha256 "097329cc381a85a85f4d7f5ee5f25057a1718b91c3f4be1623d2c0bbfc695051"
 
   depends_on "openssl@3"
