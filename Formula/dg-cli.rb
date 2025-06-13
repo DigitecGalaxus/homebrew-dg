6a1408adc1c40ce7d65eacc936ccd2932bebb8ce
Automated Account <admin@azuredevops.com>
2025-06-13 06:18:55 +0000

    Update formula with release 2.6.4

--- Formula/dg-cli.rb
+++ Formula/dg-cli.rb
@@ -2,9 +2,9 @@ class DgCli < Formula
   desc "Digitec Galaxus CLI"
   homepage "https://www.galaxus.ch"
 
-  version "2.6.3"
-  url "https://dg-package-repositories.platform.prod.int.devinite.com/DGCLI/mac/dg-cli-2.6.3.tar.gz", :using => :curl
-  sha256 "6965480d4d189a347b58e3ddfea89088a61511c9e8bcb92bd32afa1cdbace78e"
+  version "2.6.4"
+  url "https://dg-package-repositories.platform.prod.int.devinite.com/DGCLI/mac/dg-cli-2.6.4.tar.gz", :using => :curl
+  sha256 "7e3fc66d05aeaab6ef48e0aca24d9de65e4c94207078043254724efe45d3458f"
 
   depends_on "openssl@3"
   conflicts_with "dg-cli-preview", because: "cannot be installed at the same time as `dg-cli-preview`"
