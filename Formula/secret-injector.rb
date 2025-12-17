03f03b7b9b5474bd5358fc8d72c2906413fac281
Automated Account <admin@azuredevops.com>
2025-12-17 15:08:36 +0000

    Update secret-injector formula to 0.0.2

--- Formula/secret-injector.rb
+++ Formula/secret-injector.rb
@@ -2,16 +2,16 @@ class SecretInjector < Formula
   desc "Inject Azure Key Vault secrets into configuration files"
   homepage "https://github.com/DigitecGalaxus/Dg.ContainerDevelopment"
 
-  version "0.0.0"
+  version "0.0.2"
 
   on_intel do
-    url "https://dg-package-repositories.platform.prod.int.devinite.com/dg-secret-injector/mac/dg-secret-injector_0.0.0_darwin_amd64.tar.gz", using: :curl
-    sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+    url "https://dg-package-repositories.platform.prod.int.devinite.com/dg-secret-injector/mac/dg-secret-injector_0.0.2_darwin_amd64.tar.gz", using: :curl
+    sha256 "4d2347a7cf2d8d4000b0863c63213caea747541adbba819512fb0e9f6d873312"
   end
 
   on_arm do
-    url "https://dg-package-repositories.platform.prod.int.devinite.com/dg-secret-injector/mac/dg-secret-injector_0.0.0_darwin_arm64.tar.gz", using: :curl
-    sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+    url "https://dg-package-repositories.platform.prod.int.devinite.com/dg-secret-injector/mac/dg-secret-injector_0.0.2_darwin_arm64.tar.gz", using: :curl
+    sha256 "86223a795ae057721da9636c11ac219e88e58acccf2d325876dd93fa8b47f64f"
   end
 
   conflicts_with "secret-injector-preview", because: "cannot be installed at the same time as `secret-injector-preview`"
