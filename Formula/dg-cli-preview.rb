d277099943d2bdded5753771dd12b303ca57d04c
Automated Account <admin@azuredevops.com>
2026-05-07 13:58:19 +0000

    Update formula with release 2.20.0

--- Formula/dg-cli-preview.rb
+++ Formula/dg-cli-preview.rb
@@ -1,9 +1,9 @@
 class DgCliPreview < Formula
   desc "Digitec Galaxus CLI"
   homepage "https://www.galaxus.ch"
-  version "2.19.1"
-  url "https://dg-package-repositories.platform.prod.int.devinite.com/DGCLI/mac/dg-cli-2.19.1.tar.gz", :using => :curl
-  sha256 "45cb205ad0fc7b0c926253b3f3c8b89c88ddf3f9977dc4353e686dd66f203d68"
+  version "2.20.0"
+  url "https://dg-package-repositories.platform.prod.int.devinite.com/DGCLI/mac/dg-cli-2.20.0.tar.gz", :using => :curl
+  sha256 "97bd332dee8923b2bbba736739efd651f4c9551abeb9f9d7921c30a28f683507"
 
   depends_on "openssl@3"
 
--- Formula/dg-cli.rb
+++ Formula/dg-cli.rb
@@ -2,9 +2,9 @@ class DgCli < Formula
   desc "Digitec Galaxus CLI"
   homepage "https://www.galaxus.ch"
 
-  version "2.19.1"
-  url "https://dg-package-repositories.platform.prod.int.devinite.com/DGCLI/mac/dg-cli-2.19.1.tar.gz", :using => :curl
-  sha256 "45cb205ad0fc7b0c926253b3f3c8b89c88ddf3f9977dc4353e686dd66f203d68"
+  version "2.20.0"
+  url "https://dg-package-repositories.platform.prod.int.devinite.com/DGCLI/mac/dg-cli-2.20.0.tar.gz", :using => :curl
+  sha256 "97bd332dee8923b2bbba736739efd651f4c9551abeb9f9d7921c30a28f683507"
 
   depends_on "openssl@3"
   conflicts_with "dg-cli-preview", because: "cannot be installed at the same time as `dg-cli-preview`"
